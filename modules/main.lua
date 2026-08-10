8c3c1e79a6fd737b2077af55b6f289b68c4c6f29
qwertycisaTeam <159556410+qwertycisaTeam@users.noreply.github.com>
2026-08-10 23:04:23 +0600

    Update main.lua

--- modules/main.lua
+++ modules/main.lua
@@ -77,7 +77,7 @@ function Module:Init(Library, Window, Tab)
 
     -- КНОПКА ВСТАВКИ
     Tab:CreatePreviewButton({
-        Image = "rbxassetid://130841043206244", 
+        Image = "rbxassetid://129747602158533", 
         Height = 150, 
         Callback = function()
             -- Запускаем в отдельном потоке, чтобы UI не завис на время постройки
