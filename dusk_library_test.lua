c04f501ac57b963ab6da2cb306fe92a535d535f6
qwertycisaTeam <159556410+qwertycisaTeam@users.noreply.github.com>
2026-09-11 15:03:51 +0600

    Update dusk_library_test.lua

--- dusk_library_test.lua
+++ dusk_library_test.lua
@@ -1739,10 +1739,11 @@ function Library:CreateWindow(config)
                 Library.Utils.TBT(Sw, 0.25, {BackgroundColor3 = newState and Library.CurrentTheme.Accent or Library.CurrentTheme.ToggleOff})
                 Library.Utils.TBT(Kn, 0.25, {Position = newState and OnP or OffP})
                 
-                -- ФИКС: Применяем градиент при включении и удаляем при выключении
                 if newState then
+                    -- Если тоггл включили -> накидываем градиент
                     Library.Utils.ApplyGradient(Sw, Library.CurrentTheme.Accent)
                 else
+                    -- Если тоггл выключили -> убиваем старый градиент, чтобы не баговал
                     local oldGrad = Sw:FindFirstChild("DuskShine_Gradient")
                     if oldGrad then oldGrad:Destroy() end
                 end
