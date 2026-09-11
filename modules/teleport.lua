1b0339f8512bf84fd57d70adb14245dcf2d86265
qwertycisaTeam <159556410+qwertycisaTeam@users.noreply.github.com>
2026-09-11 16:29:53 +0600

    Update teleport.lua

--- modules/teleport.lua
+++ modules/teleport.lua
@@ -323,22 +323,13 @@ function Module:Init(Library, Window, Tab)
     end
 
     local function queueRefresh()
-        -- ЕСЛИ МЫ ВНУТРИ ДОМА (Y меньше 500 или больше 8500), БЛОКИРУЕМ ОБНОВЛЕНИЕ МЕНЮ
-        local char = LocalPlayer.Character
-        local hrp = char and char:FindFirstChild("HumanoidRootPart")
-        if hrp then
-            local posY = hrp.Position.Y
-            if posY < 500 or posY > 8500 then
-                return 
-            end
-        end
-
         if refreshThread then task.cancel(refreshThread) end
         refreshThread = task.spawn(function()
             task.wait(1.5) 
             updateHouseCards()
         end)
     end
+
     queueRefresh()
 
 local workspaceExteriors = workspace:WaitForChild("HouseExteriors", 5)
